4155066f777a2065a0c647ed1f5dacf918eff1c8
elisfromkirov <mozgg2002@gmail.com>
2023-09-25 13:05:48 +0300

    Initial commit

---
+++ cmake/build_options.cmake
@@ -0,0 +1,9 @@
+option(BUILD_WITH_ASAN "enable address sanitizer" OFF)
+
+option(BUILD_WITH_TSAN "enable thread sanitizer" OFF)
+
+option(BUILD_WITH_SAMPLE "enable build of samples" OFF)
+
+option(BUILD_WITH_TEST "enable build of tests" OFF)
+
+option(BUILD_WITH_BENCH "enable build of benchmarks" OFF)
